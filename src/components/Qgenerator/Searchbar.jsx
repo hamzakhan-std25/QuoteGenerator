eb7844b60e7f566ba5a3ae39e4de18c21cbb8a99
Hamza khan <hamzakhan.std25@gmail.com>
2025-08-17 12:13:24 +0500

    today work

--- src/components/Qgenerator/Searchbar.jsx
+++ src/components/Qgenerator/Searchbar.jsx
@@ -1,45 +1,93 @@
-import React from 'react'
-import { FiX } from 'react-icons/fi'
+import React, { useState } from 'react'
+
+
+
+
+
 
 
 export default function Searchbar() {
+
+
+  const obj = [
+    { id: 0, name: "inspirational", selected: false },
+    { id: 1, name: "life", selected: false },
+    { id: 2, name: "love", selected: true },
+    { id: 3, name: "happiness", selected: false },
+    { id: 4, name: "motivation", selected: false },
+    { id: 5, name: "funny", selected: true },
+    { id: 6, name: "success", selected: false },
+    { id: 7, name: "friendship", selected: false },
+    { id: 8, name: "wisdom", selected: false },
+    { id: 9, name: "philosophy", selected: false },
+    { id: 10, name: "art", selected: false },
+    { id: 11, name: "faith", selected: false },
+    { id: 12, name: "education", selected: false },
+    { id: 13, name: "politics", selected: false },
+    { id: 14, name: "sports", selected: false }
+  ];
+
+
+  const [tags, setTags] = useState(obj)
+
+
+
+
+
+
+
+  function handleSelect(id) {
+
+    setTags(
+      tags =>
+        tags.map(tag =>
+          tag.id === id ?
+            { ...tag, selected: !tag.selected } : tag))
+
+            console.log(tags);
+
+        }
+
+
+
   return (
-    <div className='grid grid-cols-[1fr_150px] mb-4 mt-4 gap-4 mr-4'>
+    <div >
+      <div className='flex mb-4 mt-4 gap-4 mr-4 justify-start items-center'>
 
-      <input type="search" placeholder='Search' className='rounded-full bg-white border border-gray-300 mr-8 flex px-4 focus:border-gray-500 focus:outline-none max-w-lg p-4 ' />
 
-      <div className='ml-auto max-w-sm'>
-        <select name="dropdown" id="dropdown" className='w-full p-2 border rounded focus:outline-none border-blue-600 text-lg ' >
-          <option value="filter1" >filter1</option>
-          <option value="filter2" selected>Filter2</option>
-          <option value="filter3">filter3</option>
-          <option value="filter4">filter4</option>
-        </select>
+        <div className='rounded-full min-w-xl bg-white border border-gray-300 flex  items-center  '>
+          <input type="search" id='search-quote' placeholder='Search' className=' flex px-4 focus:outline-none  p-4 ' />
+          <div className=' flex justify-center items-center ml-auto'>
+            <button
+              className='hover:scale-105 transition-all bg-gradient-to-r mr-4 from-blue-700 to-fuchsia-700 p-2 rounded-full text-white font-semibold flex justify-center items-center gap-2 pr-4 cursor-pointer'>Search Author</button>
+          </div>
+
+        </div>
+
+
       </div>
 
       <div className='flex gap-4 flex-wrap'>
-        <Tag tag="smoth" />
-        <Tag tag="normal" />
-        <Tag tag="normal" />
-  
+        <span className=' capitalize bg-white p-2 text-sm rounded-full w-fit px-6 h-fit border-gray-400 border  transition-colors cursor-pointer'>Filter by tags
+        </span>
+        {tags.map((tag) => {
+          return (
+            <span onClick={() => handleSelect(tag.id)}
+              key={tag.id}
+              className={`capitalize  p-2 text-sm rounded-full w-fit px-6 h-fit  transition-colors cursor-pointer ${tag.selected ? ' bg-green-300 hover:bg-green-200 ' : ' bg-gray-300 hover:bg-green-200'}`}>{tag.name}
+            </span>
+          )
+        })}
+
       </div>
-      <div className='border rounded-2xl'>
-        <select name="tags" id="tags" className='w-full p-2 rounded-2xl focus: outline-none   text-center'>
-          <option selected value="">Add Tag</option>
-          <option value="tag1">tag1</option>
-          <option value="tag1">tag1</option>
-        </select>
-      </div>
+
 
     </div>
-  )
+
+  );
 }
 
-const Tag = ({ tag }) => {
-  return (
-    <span className='bg-gray-300 p-2 text-sm rounded-full w-fit px-3 h-fit '>
-      <span className='capitalize'>{tag}</span>
-      <button className=' pl-2' ><FiX className=' inline ' /></button>
-    </span>
-  )
-}
+
+
+
+
--- src/components/Qgenerator/Topbar.jsx
+++ src/components/Qgenerator/Topbar.jsx
@@ -10,7 +10,7 @@ export default function Topbar() {
             '><FiMenu className='text-3xl' /></div>
             <h2 className=' font-bold 
             text-2xl text-transparent bg-clip-text bg-gradient-to-r to-indigo-600 from-violet-400'>Quote Generator</h2>
-            <button className=' ml-auto p-2 text-sm bg-gradient-to-r from-indigo-500 text-white to-purple-500 rounded-xl cursor-pointer font-bold '>My Favrite Quotes</button>
+            <button className='hover:scale-105 transition-all ml-auto p-2 text-sm bg-gradient-to-r from-indigo-500 text-white to-purple-500 rounded-xl cursor-pointer font-bold '>My Favrite Quotes</button>
 
 
         </div>
